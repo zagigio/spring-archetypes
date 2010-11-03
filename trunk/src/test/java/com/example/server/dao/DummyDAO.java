d521d1458dee44dea1cbf080672fbd1f4c5bc1f2
stas.fedotov <stas.fedotov@fb5c42b6-d792-f2d0-e3f6-bf3683fc75f1>
2010-11-03 15:13:34 +0000

    delete

--- trunk/src/test/java/com/example/server/dao/DummyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.dao;
-
-import com.example.server.entity.DummyEntity;
-
-/**
- * Plain DAO which provides only {@link com.example.server.dao.impl.GenericHibernateDAOImpl} methods
- */
-public interface DummyDAO extends GenericDAO<DummyEntity, Long> {
-}
